95fb39b86644eb674213432d49d21cb424eac692
xiexiaojun <xxj@1579>
2025-09-29 13:43:55 +0800

    测试提交

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ if (require('electron-squirrel-startup')) {
 }
 
 const createWindow = () => {
-  // Create the browser window.
+  // Create the browser window.  
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
